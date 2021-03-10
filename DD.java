553470a12a6a88dbce0e997be5710989b22e41c9
Sean_ L <1519059137@qq.com>
2021-03-10 20:48:33 +0800

    第一次提交

---
+++ DD.java
@@ -0,0 +1,10 @@
+public class DD{
+        public static void main(String args[]){
+                System.out.print(" \t*\t\t   \t\t*\n\n\n");
+                System.out.print("*\t\t*\t \t*\t\t*\n\n\n");
+                System.out.print(" \t \t     I Love SLD    \t\n");
+                System.out.print(" \t*\t\t \t\t*\n\n\n\n");
+                System.out.print(" \t\t*\t \t*\n\n\n\n");
+                System.out.print(" \t \t\t*\t\t");
+        }
+}
